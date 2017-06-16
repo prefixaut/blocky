0b5707d710d59b3080b5aa0d89d7830fd8a93914
PreFiXAUT <me@prefixaut.net>
2017-06-16 21:23:36 +0200

    Updated paths

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 require('vendor/autoload.php');
-require('Blocky.php');
+require('includes/Blocky.php');
+require('includes/BlockyWalker.php');
 
 $blocky = new Blocky();
 $twitch = new Kamui\API($blocky->getTwitchToken());
